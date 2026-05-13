06801c3cc3f8fa7a0396a99da964bde971581509
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-12 23:07:41 -0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Tuyen on 12/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
